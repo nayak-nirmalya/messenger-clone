25b33a46f1412b7da12510372b31de9a23b8796b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-10 12:46:39 +0530

    added new client comp AuthForm

---
+++ src/app/(site)/components/AuthForm.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const AuthForm = () => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+
+import AuthForm from "./components/AuthForm";
+
 export default function Home() {
   return (
     <div
@@ -12,7 +16,29 @@ export default function Home() {
         bg-gray-100
       "
     >
-      <div className="sm:mx-auto sm:w-full sm:max-w-md"></div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          alt="Logo"
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+          src="/images/logo.png"
+        />
+        <h2
+          className="
+            mt-6
+            text-center
+            text-3xl
+            font-bold
+            tracking-tight
+            text-gray-900
+          "
+        >
+          Sign In to Your Account
+        </h2>
+      </div>
+
+      <AuthForm />
     </div>
   );
 }
