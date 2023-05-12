c8cbd3a52f2d1bc5b93e651105ca4ab5f1e696f6
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-12 13:48:19 +0530

    added images domains to next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   experimental: {
     swcPlugins: [["next-superjson-plugin", {}]]
+  },
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com"
+    ]
   }
 };
 
