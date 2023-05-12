2b1c9d9c9748a2a58952a92e389cb16c1b4d8a3b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-12 11:13:33 +0530

    added new component EmptyState

---
+++ src/app/components/EmptyState.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const EmptyState = () => {
+  return (
+    <div
+      className="
+            px-4
+            py-10
+            sm:px-6
+            lg:px-8
+            h-full
+            flex
+            justify-center
+            items-center
+            bg-gray-100
+        "
+    >
+      <div className="text-center items-center flex flex-col">
+        <h3
+          className="
+                mt-2
+                text-2xl
+                font-bold
+                text-gray-900
+            "
+        >
+          Select a Chat or Start a New Conversation
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyState;
