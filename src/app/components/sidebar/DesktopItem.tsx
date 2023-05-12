62b69dddaa36e08e4174c2743771a2bffc830bd8
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-12 12:39:05 +0530

    added styling to desktop sidebar

--- src/app/components/sidebar/DesktopItem.tsx
+++ src/app/components/sidebar/DesktopItem.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+"use client";
 
-const DesktopItem = () => {
-  return <div>DesktopItem</div>;
+import clsx from "clsx";
+import React from "react";
+import Link from "next/link";
+
+interface DesktopItemProps {
+  label: string;
+  icon: any;
+  href: string;
+  onClick?: () => void;
+  active?: boolean;
+}
+
+const DesktopItem: React.FC<DesktopItemProps> = ({
+  href,
+  icon: Icon,
+  label,
+  onClick,
+  active
+}) => {
+  const handleClick = () => {
+    if (onClick) return onClick();
+  };
+
+  return (
+    <li onClick={handleClick}>
+      <Link
+        className={clsx(
+          `
+            group
+            flex
+            gap-x-3
+            rounded-md
+            p-3
+            text-sm
+            leading-6
+            font-semibold
+            text-gray-500
+            hover:text-black
+            hover:bg-gray-100
+        `,
+          active && "bg-gray-100 text-black"
+        )}
+        href={href}
+      >
+        <Icon className="h-6 w-6 shrink-0" />
+        <span className="sr-only">{label}</span>
+      </Link>
+    </li>
+  );
 };
 
 export default DesktopItem;
