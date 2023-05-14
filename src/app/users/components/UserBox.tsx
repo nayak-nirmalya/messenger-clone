855158405627757753773efc35d728c442c9f046
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-14 12:31:23 +0530

    added new component: UserBox

---
+++ src/app/users/components/UserBox.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { User } from "@prisma/client";
+import React from "react";
+
+const UserBox = ({ data }: { data: User }) => {
+  return <div>UserBox</div>;
+};
+
+export default UserBox;
