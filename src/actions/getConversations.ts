db4b2fbb130083b3dca873d6f6f9a798e7880aee
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-15 07:20:54 +0530

    added action to get all conversations

---
+++ src/actions/getConversations.ts
@@ -0,0 +1,30 @@
+import prisma from "@/libs/prismadb";
+import getCurrentUser from "./getCurrentUser";
+
+const getConversations = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return [];
+
+  try {
+    const conversations = await prisma.conversation.findMany({
+      orderBy: { lastMessageAt: "desc" },
+      where: {
+        userIds: { has: currentUser.id }
+      },
+      include: {
+        user: true,
+        messages: {
+          include: {
+            sender: true,
+            seen: true
+          }
+        }
+      }
+    });
+
+    return conversations;
+  } catch (error: any) {
+    return [];
+  }
+};
