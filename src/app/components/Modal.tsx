efd7d24928fb87098cdcf5665af85d3c7c4afdb1
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-17 08:04:12 +0530

    formatted code

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -25,25 +25,25 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
         >
           <div
             className="
-                        fixed
-                        inset-0
-                        bg-gray-500
-                        bg-opacity-75
-                        transition-opacity
-                    "
-          ></div>
+              fixed
+              inset-0
+              bg-gray-500
+              bg-opacity-75
+              transition-opacity
+            "
+          />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
           <div
             className="
-                flex
-                min-h-full
-                items-center
-                justify-center
-                p-4
-                text-center
-                sm:p-0
+              flex
+              min-h-full
+              items-center
+              justify-center
+              p-4
+              text-center
+              sm:p-0
             "
           >
             <Transition.Child
@@ -57,45 +57,45 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
             >
               <Dialog.Panel
                 className="
-                    relative
-                    transform
-                    overflow-hidden
-                    rounded-lg
-                    bg-white
-                    px-4 pb-4
-                    text-left
-                    shadow-xl
-                    transition-all
-                    w-full
-                    sm:my-8
-                    sm:w-full
-                    sm:max-w-lg
-                    sm:p-6
+                  relative
+                  transform
+                  overflow-hidden
+                  rounded-lg
+                  bg-white
+                  px-4 pb-4
+                  text-left
+                  shadow-xl
+                  transition-all
+                  w-full
+                  sm:my-8
+                  sm:w-full
+                  sm:max-w-lg
+                  sm:p-6
                 "
               >
                 <div
                   className="
-                        absolute
-                        right-0
-                        top-0
-                        hidden
-                        pr-4
-                        pt-4
-                        sm:block
-                        z-10
-                    "
+                    absolute
+                    right-0
+                    top-0
+                    hidden
+                    pr-4
+                    pt-4
+                    sm:block
+                    z-10
+                  "
                 >
                   <button
                     type="button"
                     className="
-                        rounded-md
-                        bg-white
-                        text-gray-400
-                        hover:text-gray-400
-                        focus:outline-none
-                        focus:ring-2
-                        focus:ring-sky-500
-                        focus:ring-offset-2
+                      rounded-md
+                      bg-white
+                      text-gray-400
+                      hover:text-gray-400
+                      focus:outline-none
+                      focus:ring-2
+                      focus:ring-sky-500
+                      focus:ring-offset-2
                     "
                     onClick={onClose}
                   >
@@ -103,7 +103,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
                     <IoClose
                       className="
                         h-6 w-6
-                    "
+                      "
                     />
                   </button>
                 </div>
