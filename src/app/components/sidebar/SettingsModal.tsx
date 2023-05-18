63393935ddef989f52d22954677f2be254598585
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-18 12:19:20 +0530

    added stylign and logic to SettingsModal

--- src/app/components/sidebar/SettingsModal.tsx
+++ src/app/components/sidebar/SettingsModal.tsx
@@ -1,9 +1,143 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { User } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { CldUploadButton } from "next-cloudinary";
 
-const SettingsModal = () => {
-  return <div>SettingsModal</div>;
+import Modal from "../Modal";
+import Input from "../inputs/Input";
+import Button from "../Button";
+
+interface SettingsModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  currentUser: User;
+}
+
+const SettingsModal: React.FC<SettingsModalProps> = ({
+  isOpen,
+  onClose,
+  currentUser
+}) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors }
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: currentUser?.name,
+      image: currentUser?.image
+    }
+  });
+
+  const image = watch("image");
+
+  const handleUpload = (result: any) => {
+    setValue("image", result?.info?.secure_url, {
+      shouldValidate: true
+    });
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/settings", data)
+      .then(() => {
+        router.refresh();
+        onClose();
+      })
+      .catch(() => toast.error("Something Went Wrong."))
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-4">
+          <div className="border-b border-gray-900/10 pb-8">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Profile
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Edit Your Public Information.
+            </p>
+
+            <div className="mt-6 flex flex-col gap-y-8">
+              <Input
+                disabled={isLoading}
+                label="Name"
+                id="name"
+                errors={errors}
+                required
+                register={register}
+              />
+              <div>
+                <label
+                  className="
+                        block
+                        text-sm
+                        font-medium
+                        leading-6
+                        text-gray-900
+                    "
+                >
+                  Photo
+                </label>
+                <div className="mt-2 items-center flex gap-x-3">
+                  <Image
+                    width="48"
+                    height="48"
+                    className="rounded-full"
+                    alt="Avatar"
+                    src={
+                      image || currentUser.image || "/images/placeholder.jpg"
+                    }
+                  />
+                  <CldUploadButton
+                    options={{ maxFiles: 1 }}
+                    onUpload={handleUpload}
+                    uploadPreset="riollf6l"
+                  >
+                    <Button disabled={isLoading} secondary type="button">
+                      Change
+                    </Button>
+                  </CldUploadButton>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="
+                mt-6
+                flex
+                items-center
+                justify-end
+                gap-x-6
+            "
+          >
+            <Button disabled={isLoading} secondary onClick={onClose}>
+              Cancel
+            </Button>
+            <Button disabled={isLoading} type="submit" onClick={onClose}>
+              Save
+            </Button>
+          </div>
+        </div>
+      </form>
+    </Modal>
+  );
 };
 
 export default SettingsModal;
