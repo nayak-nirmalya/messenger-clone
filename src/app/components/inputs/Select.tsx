9a36f59c3a85df3ff222f08a7e3e3b1ed538d6d6
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-18 12:55:02 +0530

    defined Select comp props

--- src/app/components/inputs/Select.tsx
+++ src/app/components/inputs/Select.tsx
@@ -2,7 +2,15 @@
 
 import React from "react";
 
-const Select = () => {
+interface SelectProps {
+  disabled?: boolean;
+  label: string;
+  value?: Record<string, any>;
+  onChange: (value: Record<string, any>) => void;
+  options: Record<string, any>;
+}
+
+const Select: React.FC<SelectProps> = ({ label, onChange, options, value }) => {
   return <div>Select</div>;
 };
 
