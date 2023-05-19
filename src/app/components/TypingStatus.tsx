4d52e5ea3eea08152d3539a52080c6c5e2ca1543
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-19 11:18:13 +0530

    created new component TypingStatus

---
+++ src/app/components/TypingStatus.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+// import useActiveChannel from "@/hooks/useActiveChannel";
+
+const TypingStatus = () => {
+  // useActiveChannel();
+
+  return null;
+};
+
+export default TypingStatus;
