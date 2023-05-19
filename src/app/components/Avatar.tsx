c1424ce68591cfb053327c217610482b4c15f5b1
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-19 07:53:18 +0530

    show user active status conditionaly in avatar

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 import React from "react";
 
 import { User } from "@prisma/client";
+import useActiveList from "@/hooks/useActiveList";
 
 interface AvatarProps {
   user: User;
 }
 
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
+  const { members } = useActiveList();
+  const isActive = members.indexOf(user?.email!) !== -1;
+
   return (
     <div className="relative">
       <div
@@ -26,8 +30,9 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
           fill
         />
       </div>
-      <span
-        className="
+      {isActive && (
+        <span
+          className="
             absolute
             block
             rounded-full
@@ -38,8 +43,9 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
             right-0
             h-2 w-2
             md:h-3 md:w-3
-        "
-      />
+          "
+        />
+      )}
     </div>
   );
 };
