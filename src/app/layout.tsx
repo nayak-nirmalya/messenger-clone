afe1477320df896668e41ff3f5be6488d9a31990
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-19 07:38:34 +0530

    added hook: useActiveChannel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import ToasterContext from "@/context/ToasterContext";
 import AuthContext from "@/context/AuthContext";
+import ActiveStatus from "./components/ActiveStatus";
 
 export const metadata = {
   title: "Messenger Clone",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <AuthContext>
           <ToasterContext />
+          <ActiveStatus />
           {children}
         </AuthContext>
       </body>
